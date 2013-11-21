405422c57b829bf3d6c193b87f16632c663017ec
Philip Hutchins <flipture@gmail.com>
2013-11-21 12:26:43 -0500

    adding wrapper for mysql on opsworks

---
+++ mysql-opsworks/attributes/mysql.rb
@@ -0,0 +1,2 @@
+default['mysql']['server_debian_password'] = 'd3a1erMat3h'
+default['mysql']['server_repl_password'] = 'd3a1erMat3h'
---
+++ mysql-opsworks/metadata.rb
@@ -0,0 +1,8 @@
+name		          'mysql-opsworks'
+maintainer        'Philip Hutchins'
+maintainer_email  'philip.hutchins@dealermatch.com'
+description       'Custom configuration for MySQL on Opsworks'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.0.1'
+
+depends 'mysql'
