8cfa0e9946413290cbba5861fff5cadf38b66f13
Philip Hutchins <flipture@gmail.com>
2013-11-27 13:35:58 -0500

    adding env stuff

--- corndog-chef/recipes/dotenv.rb
+++ corndog-chef/recipes/dotenv.rb
@@ -3,12 +3,6 @@ gem_package 'dotenv-rails' do
   action :install
 end
 
-#f = File.open("#{corndog_path}/.env",'w')
-#node['corndog']['env'].each do |key, value|
-#  f.write("#{key}=#{value}")
-#end
-#f.close
-
 template "#{corndog_path}/.env" do
   source "dotenv.erb"
   owner "root"
--- corndog-chef/recipes/prod.rb
+++ corndog-chef/recipes/prod.rb
@@ -1,4 +1,15 @@
 node.normal['scope'] = 'prod'
+node.normal['profile'] = 'production'
+node.normal['profile'] = 'test'
+
+# modifying hostnames to ensure no colision with actual production
+node.normal['corndog']['mongodb']['hosts'] = [
+  "test1-cde-production.member.mongolayer.com:27017",
+  "test2-cde-production.member.mongolayer.com:27017" ]
+#  "cde-production.member2.mongolayer.com:27017",
+#  "cde-production.member3.mongolayer.com:27017" ]
+node.normal['corndog']['mongodb']['database'] = 'cde_production'
 
 include_recipe 'corndog-chef::default'
 include_recipe 'corndog-chef::profile-production'
+include_recipe 'corndog-chef::dottenv'
