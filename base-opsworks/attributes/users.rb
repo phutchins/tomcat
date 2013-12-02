0504ae9dae7d186728918002ef56c5cb6f461cd1
Philip Hutchins <flipture@gmail.com>
2013-12-02 12:19:14 -0500

    adding new base

--- base-opsworks/attributes/users.rb
+++ base-opsworks/attributes/users.rb
@@ -1,3 +1,3 @@
 default['authorization']['sudo']['groups'] = ['devops', 'ubuntu']
-default['authorization']['sudo']['users'] = ['ubuntu', 'phutchins', 'jgerry']
+default['authorization']['sudo']['users'] = ['ubuntu', 'phutchins', 'jgerry', 'deploy']
 default['authorization']['sudo']['passwordless'] = true
