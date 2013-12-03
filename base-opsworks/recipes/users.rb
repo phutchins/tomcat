192455ce7d42d337aa35a8b12d2dd92e0a44b1e6
Philip Hutchins <flipture@gmail.com>
2013-12-03 15:01:14 -0500

    fixing sudoers

--- base-opsworks/recipes/users.rb
+++ base-opsworks/recipes/users.rb
@@ -4,6 +4,7 @@ end
 include_recipe "base-opsworks::user-phutchins"
 include_recipe "base-opsworks::user-jgerry"
 
+node.default['authorization']['sudo']['prefix'] = '/etc'
 include_recipe "sudo"
 
 include_recipe "base-opsworks::sudoers"
