7636570d119d4e6248424f7a1fffa35e71136885
Philip Hutchins <flipture@gmail.com>
2013-12-05 13:48:26 -0500

    Updating corndog-chef

--- corndog-chef/recipes/dotenv.rb
+++ corndog-chef/recipes/dotenv.rb
@@ -1,4 +1,4 @@
-corndog_path = File.join(node['corndog']['base_path'], node['deploy']['Corndog_APP']['symlinks']['current'])
+corndog_path = File.join(node['corndog']['base_path'], "current")
 gem_package 'dotenv-rails' do
   action :install
 end
@@ -12,6 +12,26 @@ template "#{corndog_path}/.env" do
   variables(
     :dot_env_attrs => node['corndog']['dotenv']
   )
+}
+
+template "/tmp/.env" do
+  source "dotenv.erb"
+  owner "root"
+  group "root"
+  variables(
+    :dot_env_attrs => node['corndog']['dotenv']
+  )
+}
+
+template "/tmp/test.env" do
+  source "dotenv.erb"
+  owner "root"
+  group "root"
+  variables(
+    :dot_env_attrs => node['corndog']['dotenv']
+  )
+}
+
 # Do we need to restart unicorn for environment variable changes?
 #  notifies :restart, resources(:service => "unicorn")
 end
