ec6395ed151c0de8dc6be2e6e6286af36de68500
Philip Hutchins <flipture@gmail.com>
2013-12-06 12:22:02 -0500

    Updating corndog-chef

--- corndog-chef/recipes/devopstest.rb
+++ corndog-chef/recipes/devopstest.rb
@@ -6,7 +6,8 @@ node.normal['corndog']['dotenv'] = {
   'RAILS_ENV' => 'devopstest',
   'MONGODB_HOST' => 'mongo-devopstest.dealermatch.biz:27017',
   'MONGODB_DATABASE' => 'cde_production',
-  'MONGODB_OPTIONS' => '*mongo_session_options_with_ro_username_pass'
+  'MONGODB_OPTIONS' => '*mongo_session_options_with_ro_username_pass',
+  'REDIS_URI' => 'redis://redis-corndog-prod.zx3kal.0001.use1.cache.amazonaws.com:6379'
 # Will want to grab the MySQL master from OpsWorks attributes later
 #  'MYSQL_MASTER_HOST' => node[:opsworks][:layers]['db-master'][:instances][:
 }
