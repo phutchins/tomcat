06ac188219f7764879e5741795fdd32f736ad19e
Philip Hutchins <flipture@gmail.com>
2013-12-06 14:32:39 -0500

    Updating corndog-chef

--- corndog-chef/recipes/devopstest.rb
+++ corndog-chef/recipes/devopstest.rb
@@ -4,8 +4,9 @@ include_recipe 'corndog-chef::profile-testing'
 
 # Attributes from the OpsWorks Environment
 redis_uri_port = "6379"
+redis_uri = 'redis://'
 node['opsworks']['layers']['redis'].each do |instance|
-  redis_uri_hosts << instance['public_dns_name'] + $redis_port
+  redis_uri << instance['public_dns_name'] + $redis_port
 end
 
 Chef::Log.info("Redis URI: redis://#{redis_uri_hosts}")
@@ -16,7 +17,7 @@ node.normal['corndog']['dotenv'] = {
   'MONGODB_DATABASE' => 'cde_production',
   'MONGODB_OPTIONS' => '*mongo_session_options_with_ro_username_pass',
 #  'REDIS_URI' => 'redis://redis-corndog-prod.zx3kal.0001.use1.cache.amazonaws.com:6379'
-  'REDIS_URI' => "redis://#{$redis_uri_hosts}"
+  'REDIS_URI' => redis_uri
 # Will want to grab the MySQL master from OpsWorks attributes later
 #  'MYSQL_MASTER_HOST' => node[:opsworks][:layers]['db-master'][:instances][:
 }
