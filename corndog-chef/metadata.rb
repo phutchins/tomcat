0be5a7cf34543d9d9afded0940b108998b91c20d
Philip Hutchins <flipture@gmail.com>
2013-12-06 14:19:22 -0500

    Updating corndog-chef

--- corndog-chef/metadata.rb
+++ corndog-chef/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'philip.hutchins@dealermatch.com'
 license          'All rights reserved'
 description      'Configures and Deploys Corndog'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version     '0.0.6'
+version     '0.0.7'
--- corndog-chef/recipes/devopstest.rb
+++ corndog-chef/recipes/devopstest.rb
@@ -2,12 +2,21 @@ node.normal['corndog']['scope'] = 'devopstest'
 node.normal['deploy']['Corndog_APP']['rails_env'] = 'devopstest'
 include_recipe 'corndog-chef::profile-testing'
 
+# Attributes from the OpsWorks Environment
+redis_uri_port = "6379"
+node['opsworks']['layers']['redis'].each do |instance|
+  redis_uri_hosts << instance['public_dns_name'] + $redis_port
+end
+
+Chef::Log.info("Redis URI: redis://#{redis_uri_hosts}")
+
 node.normal['corndog']['dotenv'] = { 
   'RAILS_ENV' => 'devopstest',
   'MONGODB_HOST' => 'mongo-devopstest.dealermatch.biz:27017',
   'MONGODB_DATABASE' => 'cde_production',
   'MONGODB_OPTIONS' => '*mongo_session_options_with_ro_username_pass',
-  'REDIS_URI' => 'redis://redis-corndog-prod.zx3kal.0001.use1.cache.amazonaws.com:6379'
+#  'REDIS_URI' => 'redis://redis-corndog-prod.zx3kal.0001.use1.cache.amazonaws.com:6379'
+  'REDIS_URI' => "redis://#{$redis_uri_hosts}"
 # Will want to grab the MySQL master from OpsWorks attributes later
 #  'MYSQL_MASTER_HOST' => node[:opsworks][:layers]['db-master'][:instances][:
 }
