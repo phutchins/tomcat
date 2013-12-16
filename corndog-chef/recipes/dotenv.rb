4fe35b261743b81afcf78b006009010233516d1e
Philip Hutchins <flipture@gmail.com>
2013-12-16 13:22:59 -0500

    Updating corndog-chef

--- corndog-chef/recipes/dotenv.rb
+++ corndog-chef/recipes/dotenv.rb
@@ -17,5 +17,5 @@ template "#{corndog_shared_path}/config/dotenv" do
   variables(
     :dot_env_attrs => node['corndog']['dotenv']
   )
-  only_if { ::Dir.exists?(corndog_shared_path) }
+  only_if { ::Dir.exist?(corndog_shared_path) }
 end
