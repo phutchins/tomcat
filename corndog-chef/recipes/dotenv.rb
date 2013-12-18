2631bf93c4667b0eb24ce7ca14bad264476a7dd7
Philip Hutchins <flipture@gmail.com>
2013-12-18 11:10:28 -0500

    Updating corndog-chef

--- corndog-chef/recipes/dotenv.rb
+++ corndog-chef/recipes/dotenv.rb
@@ -19,6 +19,15 @@ gem_package 'dotenv-rails' do
   action :install
 end
 
+#link "#{release_path}/.env" do
+#  to "/opt/corndog/dotenv"
+#end
+
+#execute "symlinking dotenv config if necessary" do
+#  command "rm #{release_path}/.env && ln -s /opt/corndog/dotenv #{release_path}/.env"
+#  action :run
+#end
+
 Chef::Log.info("DotEnv Attrs: #{node['corndog']['dotenv'].inspect}")
 
 #if !::File.directory?(corndog_shared_path)
