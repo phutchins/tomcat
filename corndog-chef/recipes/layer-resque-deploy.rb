ae94fa7dd0f344b6a03b7d2d94705105c2ac6f4b
Jason Gerry <jason.gerry@gmail.com>
2013-12-23 14:51:16 -0500

    don't have deploy user info before it's created

--- corndog-chef/recipes/layer-resque-deploy.rb
+++ corndog-chef/recipes/layer-resque-deploy.rb
@@ -1,27 +1,29 @@
-template "/etc/bluepill_resque.pill" do
-  owner 'root'
-  group 'root'
-  mode 0644
-  source 'resque.rb.erb'
-  variables({
-    :rails_env => node['deploy']['corndog']['rails_env']
-  })
-  notifies :run, "execute[resque-start]"
+# only run after deploy (and deploy user) has occurred
+if node['deploy']['corndog']['rails_env']
+  template "/etc/bluepill_resque.pill" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    source 'resque.rb.erb'
+    variables({
+      :rails_env => node['deploy']['corndog']['rails_env']
+    })
+    notifies :run, "execute[resque-start]"
+  end
+
+  log "RAILS_ENV set to: #{node['deploy']['corndog']['rails_env']}"
+
+  execute "resque-stop" do
+    user "root"
+    command "bluepill resque stop"
+    only_if "ps -ef | egrep resque | egrep -v egrep | egrep -v bluepilld"
+    action :run
+  end
+
+  execute "resque-start" do
+    user "root"
+    command "bluepill load /etc/bluepill_resque.pill"
+    not_if "ps -ef | egrep resque | egrep -v egrep | egrep -v bluepilld"
+    action :run
+  end
 end
-
-log "RAILS_ENV set to: #{node['deploy']['corndog']['rails_env']}"
-
-execute "resque-stop" do
-  user "root"
-  command "bluepill resque stop"
-  only_if "ps -ef | egrep resque | egrep -v egrep | egrep -v bluepilld"
-  action :run
-end
-
-execute "resque-start" do
-  user "root"
-  command "bluepill load /etc/bluepill_resque.pill"
-  not_if "ps -ef | egrep resque | egrep -v egrep | egrep -v bluepilld"
-  action :run
-end
-
