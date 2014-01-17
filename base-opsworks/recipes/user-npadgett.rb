fe76b885d9e69f1df569250c0bb56cf7da347a6e
Philip Hutchins <flipture@gmail.com>
2014-01-17 11:03:27 -0500

    adding nicks user

---
+++ base-opsworks/recipes/user-npadgett.rb
@@ -0,0 +1,32 @@
+user "npadgett" do
+  comment "Nick Padgett"
+  uid 1005
+  gid 500
+  home "/home/npadgett"
+  shell "/bin/bash"
+end
+
+["/home/npadgett", "/home/npadgett/.ssh"].each do |dir|
+  directory dir do
+    mode 0700
+    owner "npadgett"
+    group "devops"
+    action :create
+  end
+end
+
+cookbook_file "/home/npadgett/.ssh/authorized_keys" do
+  source "authorized_keys.npadgett"
+  mode 0600
+  owner "npadgett"
+  group "devops"
+  action :create
+end
+
+cookbook_file "/etc/sudoers.d/npadgett" do
+  source "npadgett-sudo"
+  mode 0440
+  owner "root"
+  group "root"
+  action :create
+end
