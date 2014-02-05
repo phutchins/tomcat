af6b5d3f40c41432d8b243c5a11c2b043a10a4e7
Philip Hutchins <flipture@gmail.com>
2014-02-05 15:12:16 -0500

    putting cron in block

--- corndog-chef/recipes/resque-cron.rb
+++ corndog-chef/recipes/resque-cron.rb
@@ -1,1 +1,4 @@
-run "cd #{node['corndog']['app']['base_path']} && bundle exec whenever --write-crontab --set environment=#{node['corndog']['profile']}"
+execute "run-rake-cron" do
+  user "root"
+  command "cd #{node['corndog']['app']['base_path']} && bundle exec whenever --write-crontab --set environment=#{node['corndog']['profile']}"
+end
