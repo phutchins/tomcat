26abddcc3d230b978e6dd9082e49d21d94333ebc
Philip Hutchins <flipture@gmail.com>
2014-02-06 15:38:10 -0500

    fixing attr namespace

--- corndog-chef/recipes/dotenv.rb
+++ corndog-chef/recipes/dotenv.rb
@@ -1,6 +1,6 @@
-default['corndog']['app']['base_path'] = '/srv/www/corndog'
-default['corndog']['app']['current_link'] = '/srv/www/corndog/current'
-default['corndog']['app']['shared_path'] = '/srv/www/corndog/shared'
+node['corndog']['app']['base_path'] = '/srv/www/corndog'
+node['corndog']['app']['current_link'] = '/srv/www/corndog/current'
+node['corndog']['app']['shared_path'] = '/srv/www/corndog/shared'
 
 directory "/opt/corndog" do
   owner "deploy"
