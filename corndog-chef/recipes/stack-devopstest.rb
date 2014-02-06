2b694db2953da335b16e418ecd6b1a94915622db
Philip Hutchins <flipture@gmail.com>
2014-02-06 17:14:49 -0500

    fixing cloudfront url

--- corndog-chef/recipes/stack-devopstest.rb
+++ corndog-chef/recipes/stack-devopstest.rb
@@ -5,7 +5,7 @@ run_context.include_recipe 'corndog-chef::stack-defaults'
 # This file includes all attributes that apply only to the devopstest stack
 
 # Assign environmentals to node
-node.normal['corndog']['cloudfront']['asset_hosts'] = 'https://d2tb02q5zn0zzy.cloudfront.net'
+node.normal['corndog']['cloudfront']['asset_hosts'] = 'https://d5a12sbnyk1s7.cloudfront.net'
 
 node.normal['corndog']['mongodb']['host_port_1'] = 'cde-prod2.m0.mongolayer.com:27017'
 node.normal['corndog']['mongodb']['host_port_2'] = 'cde-prod2.m1.mongolayer.com:27017'
