b1d5485806e66053ee565e08882a604bbe718103
Philip Hutchins <flipture@gmail.com>
2014-02-06 16:51:10 -0500

    fixing corndog current directory

--- corndog-chef/recipes/resque-cron.rb
+++ corndog-chef/recipes/resque-cron.rb
@@ -1,4 +1,4 @@
 execute "run-rake-cron" do
   user "root"
-  command "cd #{node['corndog']['app']['base_path']} && bundle exec whenever --write-crontab --set environment=#{node['corndog']['profile']}"
+  command "cd #{node['corndog']['app']['base_path']}/current && bundle exec whenever --write-crontab --set environment=#{node['corndog']['profile']}"
 end
