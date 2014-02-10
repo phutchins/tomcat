db3f62c3185cd2af77539e5f025ce38a54399c46
Philip Hutchins <flipture@gmail.com>
2014-02-10 12:08:03 -0500

    changing cron user to deploy

--- corndog-chef/recipes/resque-cron.rb
+++ corndog-chef/recipes/resque-cron.rb
@@ -1,4 +1,4 @@
 execute "run-rake-cron" do
-  user "root"
+  user "deploy"
   command "cd #{node['corndog']['app']['base_path']}/current && bundle exec whenever --write-crontab --set environment=#{node['corndog']['profile']}"
 end
