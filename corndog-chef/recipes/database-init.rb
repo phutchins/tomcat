75d197078eb5885da5b844ab11ff53f54093b6e3
Jason Gerry <jason.gerry@gmail.com>
2014-02-20 14:17:21 -0500

    add execute resources for KBB fix

--- corndog-chef/recipes/database-init.rb
+++ corndog-chef/recipes/database-init.rb
@@ -1,31 +1,33 @@
 config_path = node['corndog']['db']['config_path']
 config_environment = node['scope']
 
-def database_config
-  @config ||= YAML.load_file("#{config_path}/database.yml")[config_environment]
-end
-
-def mysql_exec statement
-  #cmd = "ssh -o 'StrictHostKeyChecking no' -i /home/deploy/.ssh/internal #{node['corndog']['db']['host']} \"mysql -uroot -e '#{statement}'\""
-  cmd = "mysql -uroot -p#{database_config.password} -e '#{statement}'"
-  puts "Running: #{cmd}"
-  run cmd
-end
-
-# Commenting out the conditional and using OpsWorks configure lifecycle to control when this is run
-#if !node['corndog']['db']['init_complete']
-  begin
-    Chef::Log.info("Remote Database is: #{database_config}")
-
-    Chef::Log.info("granting access on kbb_etl to deploy")
-    mysql_exec "create database if not exists kbb_etl"
-    mysql_exec 'GRANT ALL ON kbb_etl.* TO \"deploy\"@\"%\";'
-
-    Chef::Log.info("running migration script for etl load target")
-    mysql_exec "create database if not exists etl_load_target"
-    mysql_exec 'grant all on etl_load_target.* to \"deploy\"@\"%\";'
-    node.normal['corndog']['db']['init_complete'] = true
-  rescue Exception => e
-    Chef::Log.fatal("Cannot initialize MySQL Database for Corndog!")
-  end
+#def database_config
+#  @config ||= YAML.load_file("#{config_path}/database.yml")[config_environment]
 #end
+execute "create kbb_etl database" do
+  user = 'deploy'
+  command "echo 'create database if not exists kbb_etl' | mysql"
+  not_if = "echo 'show databases' | mysql | egrep kbb_etl"
+  Chef::Log.info("creating kbb_etl database")
+end
+
+execute "grants for kbb_etl database" do
+  user = 'deploy'
+  command "echo 'GRANT ALL ON kbb_etl.* TO \"deploy\"@\"%\"' | mysql"
+  Chef::Log.info("granting roles on kbb_etl to deploy")
+end
+
+execute "create etl_load_target database" do
+  user = "deploy"
+  command "echo 'create database if not exists etl_load_target' | mysql"
+  not_if = "echo 'show databases' | mysql | egrep etl_load_target"
+  Chef::Log.info("creating etl_load_target database")
+end
+
+execute "grants for etl_load_target database" do
+  user = "deploy"
+  command "echo 'grant all on etl_load_target.* to \"deploy\"@\"%\"' | mysql"
+  Chef::Log.info("granting roles on etl_load_target to deploy")
+end
+
+
