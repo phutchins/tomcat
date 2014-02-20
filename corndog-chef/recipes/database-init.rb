d8649c39e1d100d126f92ae5c9ebed989376c387
Jason Gerry <jason.gerry@gmail.com>
2014-02-20 14:32:30 -0500

    password update for MySQL root user

--- corndog-chef/recipes/database-init.rb
+++ corndog-chef/recipes/database-init.rb
@@ -1,32 +1,31 @@
 config_path = node['corndog']['db']['config_path']
 config_environment = node['scope']
 
-#def database_config
-#  @config ||= YAML.load_file("#{config_path}/database.yml")[config_environment]
-#end
+dbcall = "mysql -u root -p#{node[:mysql][:server_root_password]}"
+
 execute "create kbb_etl database" do
   user = 'deploy'
   command "echo 'create database if not exists kbb_etl' | mysql"
-  not_if = "echo 'show databases' | mysql | egrep kbb_etl"
+  not_if = "echo 'show databases' | #{dbcall} | egrep kbb_etl"
   Chef::Log.info("creating kbb_etl database")
 end
 
 execute "grants for kbb_etl database" do
   user = 'deploy'
-  command "echo 'GRANT ALL ON kbb_etl.* TO \"deploy\"@\"%\"' | mysql"
+  command "echo 'GRANT ALL ON kbb_etl.* TO \"deploy\"@\"%\"' | #{dbcall}"
   Chef::Log.info("granting roles on kbb_etl to deploy")
 end
 
 execute "create etl_load_target database" do
   user = "deploy"
   command "echo 'create database if not exists etl_load_target' | mysql"
-  not_if = "echo 'show databases' | mysql | egrep etl_load_target"
+  not_if = "echo 'show databases' | #{dbcall} | egrep etl_load_target"
   Chef::Log.info("creating etl_load_target database")
 end
 
 execute "grants for etl_load_target database" do
   user = "deploy"
-  command "echo 'grant all on etl_load_target.* to \"deploy\"@\"%\"' | mysql"
+  command "echo 'grant all on etl_load_target.* to \"deploy\"@\"%\"' | #{dbcall}"
   Chef::Log.info("granting roles on etl_load_target to deploy")
 end
 
