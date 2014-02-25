b99b9d1096402ea7b2d04ae401ba5169ca38b032
Philip Hutchins <flipture@gmail.com>
2014-02-25 17:16:25 -0500

    removing nginx permission change from base logstash recipe

--- base-opsworks/recipes/monitoring-logstash.rb
+++ base-opsworks/recipes/monitoring-logstash.rb
@@ -2,11 +2,4 @@ include_recipe 'openssl'
 
 node.default['logstash']['agent']['server_ipaddress'] = "logs.dealermatch.biz"
 
-script "make logs readable" do
-  interpreter "bash"
-  user "root"
-  cwd "/var/log"
-  code "chmod -R 664 /var/log/nginx"
-end
-
 include_recipe "logstash::agent"
