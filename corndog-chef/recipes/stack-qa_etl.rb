1c58f37293fa114e58dd666206a5d722b68507b2
Philip Hutchins <flipture@gmail.com>
2014-03-05 15:38:08 -0500

    fixing cloudfront for qa_etl

--- corndog-chef/recipes/stack-qa_etl.rb
+++ corndog-chef/recipes/stack-qa_etl.rb
@@ -8,6 +8,8 @@ run_context.include_recipe 'corndog-chef::stack-defaults'
 node.normal['corndog']['mongodb']['host_port_1'] = 'mongo3.dealermatch.biz:27017'
 node.normal['corndog']['mongodb']['database'] = 'cde_production'
 
+nocd.normal['corndog']['cloudfront']['asset_hosts'] = "https://qa-etl.dealermatch.biz"
+
 node.normal['corndog']['solr']['path'] = '/solr/eaa64a9f345'
 node.normal['corndog']['solr']['master_path'] = '/solr/dc2f508b356'
 
