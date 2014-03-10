27d0f216c2efa5141db3b9393d310604e0eb480b
Philip Hutchins <flipture@gmail.com>
2014-03-10 12:10:06 -0400

    adding upgrade recipe for nginx to 1.4.5

---
+++ corndog-chef/recipes/layer-rails-setup.rb
@@ -0,0 +1,16 @@
+include_recipe 'apt'
+
+apt_repository "nginx" do
+  uri "http://ppa.launchpad.net/nginx"
+  distribution "precise"
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "C300EE8C"
+  action :add
+  notifies :run, "execute[apt-get update]", :immediately
+end
+
+apt_package "nginx" do
+  action :install
+  version "1.4.5-1+precise0"
+end
