d7dcd9b35dd0159b11ffd0804132ccc1f1d398cf
Jason Gerry <jason.gerry@gmail.com>
2014-03-20 09:18:05 -0400

    fixing more directory existence bugs

--- unicorn/recipes/config-nginx.rb
+++ unicorn/recipes/config-nginx.rb
@@ -3,6 +3,7 @@ cookbook_file "log_format" do
   path '/etc/nginx/conf.d/log_format.conf'
   owner 'root'
   group 'root'
+  only_if "test -d /etc/nginx/conf.d"
   mode 0644
 end
 
@@ -14,6 +15,7 @@ template "/etc/nginx/sites-available/corndog" do
   variables({
     :hostname => node[:opsworks][:instance][:hostname]
   })
+  only_if "test -d /etc/nginx/sites-available"
   notifies :restart, "service[nginx]"
 end
 
