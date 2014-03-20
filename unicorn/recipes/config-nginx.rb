ac45e0bef881ec0defcde2e1d7e1049ddf3d9e5a
Jason Gerry <jason.gerry@gmail.com>
2014-03-20 09:33:28 -0400

    wrap nginx configs with conditional to exclude resque layer

--- unicorn/recipes/config-nginx.rb
+++ unicorn/recipes/config-nginx.rb
@@ -1,25 +1,27 @@
-# nginx config
-cookbook_file "log_format" do
-  path '/etc/nginx/conf.d/log_format.conf'
-  owner 'root'
-  group 'root'
-  only_if "test -d /etc/nginx/conf.d"
-  mode 0644
-end
+if ! node[:opsworks][:layers]['resque']
+  # nginx config
+  cookbook_file "log_format" do
+    path '/etc/nginx/conf.d/log_format.conf'
+    owner 'root'
+    group 'root'
+    only_if "test -d /etc/nginx/conf.d"
+    mode 0644
+  end
 
-template "/etc/nginx/sites-available/corndog" do
-  owner 'root'
-  group 'root'
-  mode 0644
-  source 'nginx.conf.erb'
-  variables({
-    :hostname => node[:opsworks][:instance][:hostname]
-  })
-  only_if "test -d /etc/nginx/sites-available"
-  notifies :restart, "service[nginx]"
-end
+  template "/etc/nginx/sites-available/corndog" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    source 'nginx.conf.erb'
+    variables({
+      :hostname => node[:opsworks][:instance][:hostname]
+    })
+    only_if "test -d /etc/nginx/sites-available"
+    notifies :restart, "service[nginx]"
+  end
 
-service "nginx" do
-  action [ :enable, :start ]
+  service "nginx" do
+    action [ :enable, :start ]
+  end
 end
 
