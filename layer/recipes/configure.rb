b429eb540f394ae3337e2e0c36aaa79ec83db7f5
Jason Gerry <jason.gerry@gmail.com>
2014-03-24 16:52:30 -0400

    Revert "first try at including all chef in single recipe"
    This reverts commit 28708aaa13e69a494465c6b77c5ffe03cdff1fb4.

--- layer/recipes/configure.rb
+++
@@ -1,6 +0,0 @@
-include_recipe 'base-opsworks::monitoring-app'
-include_recipe 'base-opsworks'
-include_recipe 'unicorn::config-nginx'
-include_recipe 'corndog-chef::layer-rails-configure'
-
-
--- layer/recipes/setup.rb
+++
@@ -1,4 +0,0 @@
-include_recipe 'corndog-chef::app-packages'
-include_recipe 'corndog-chef::layer-rails-setup'
-include_recipe 'corndog-chef::layer-rails-configure'
-
