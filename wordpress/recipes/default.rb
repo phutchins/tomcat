1b88d9d89d89d72ebf5ee8be1b38760044fb1096
Jason Gerry <jason.gerry@gmail.com>
2014-03-25 18:12:44 -0400

    adding wordpress testing

---
+++ wordpress/recipes/default.rb
@@ -0,0 +1,48 @@
+stack = node[:wordpress][:stack]
+
+# override attributes
+node.override[:apache][:prefork][:minspareservers] = 2
+node.override[:apache][:prefork][:maxspareservers] = 4
+node.override[:apache][:prefork][:startservers] = 4
+##
+
+dbuser = 'master'
+dbpass = 'D0nkeyc0rn!'
+dbhost = 'cdx-wordpress.c4fsy8hm6tiy.us-east-1.rds.amazonaws.com'
+
+case
+when stack == 'test'
+  dbname = 'wptest'
+when stack == 'production'
+  dbname = 'wordpress'
+end
+
+template "wp-config.php" do
+  source = 'wp-config.php.erb'
+  user   = 'www-data'
+  group  = 'www-data'
+  mode   = 0644
+  path   = '/srv/www/wordpress/current/wp-config.php'
+  notifies :restart, "service[apache2]", :immediately
+  variables({
+    :dbname => dbname,
+    :dbuser => dbuser,
+    :dbpass => dbpass,
+    :dbhost => dbhost
+  })
+end
+
+#cookbook_file "wp-config.php" do
+  #user  = 'www-data'
+  #group = 'www-data'
+  #mode  = 0644
+  #path  = '/srv/www/wordpress/current/wp-config.php'
+  #source = "wp-config.php.#{stack}"
+  #notifies :restart, "service[apache2]", :immediately
+#end
+
+service "apache2" do
+  init_command = '/etc/init.d/apache2'
+  action = :nothing
+end
+
