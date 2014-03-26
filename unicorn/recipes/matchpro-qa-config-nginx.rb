bcccb86eff2243af80710f7cde2392114580ee88
Jason Gerry <jason.gerry@gmail.com>
2014-03-26 14:19:32 -0400

    add matchpro nginx config for http redirect to https

---
+++ unicorn/recipes/matchpro-qa-config-nginx.rb
@@ -0,0 +1,23 @@
+# nginx config
+cookbook_file "log_format" do
+  path '/etc/nginx/conf.d/log_format.conf'
+  owner 'root'
+  group 'root'
+  mode 0644
+end
+
+template "/etc/nginx/sites-available/matchpro" do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source 'matchpro.nginx.conf.erb'
+  variables({
+    :hostname => node[:opsworks][:instance][:hostname]
+  })
+  notifies :restart, "service[nginx]"
+end
+
+service "nginx" do
+  action [ :enable, :start ]
+end
+
