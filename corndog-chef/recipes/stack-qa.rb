799f6aeee6a0e8a0587bbb95b57d2936af05b0aa
Jason Gerry <jason.gerry@gmail.com>
2014-03-31 16:58:38 -0400

    changing qa config for new mongo db

--- corndog-chef/recipes/stack-qa.rb
+++ corndog-chef/recipes/stack-qa.rb
@@ -7,7 +7,7 @@ run_context.include_recipe 'corndog-chef::stack-defaults'
 # Assign environmentals to node
 node.normal['corndog']['cloudfront']['asset_hosts'] = 'https://d32lfm0jr6byac.cloudfront.net'
 
-node.normal['corndog']['mongodb']['host_port_1'] = 'c0.cde-qa.m0.mongolayer.com:27017'
+node.normal['corndog']['mongodb']['host_port_1'] = 'mongo-qa.dealermatch.biz:27017'
 node.normal['corndog']['mongodb']['database'] = 'cde_production'
 
 node.normal['corndog']['solr']['path'] = '/solr/e3cb33a92d8'
