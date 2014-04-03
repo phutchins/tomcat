1802fff4e21b3480dc5b32f4460e316197a4ea85
Philip Hutchins <flipture@gmail.com>
2014-04-03 11:08:54 -0400

    adding nginx fix here also

--- corndog-chef/recipes/layer-rails-setup.rb
+++ corndog-chef/recipes/layer-rails-setup.rb
@@ -1,5 +1,14 @@
 include_recipe 'apt'
 
+%w{nginx-full_1.4.6-1+precise0_amd64.deb nginx-common_1.4.6-1+precise0_all.deb nginx_1.4.6-1+precise0_all.deb}.each do |pkg|
+s3_file "/var/cache/apt/archives/#{pkg}" do
+  remote_path "/#{pkg}"
+  bucket "cdx-apt-packages"
+  aws_access_key_id "AKIAJ4TP2AWG2GYM6V6Q"
+  aws_secret_access_key "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"
+  not_if { File.exists?("/var/cache/apt/archives/#{pkg}") }
+end
+
 apt_repository "nginx-stable-precise" do
   uri "http://ppa.launchpad.net/nginx/stable/ubuntu"
   distribution "precise"
