f19beed73b52b8ff1d17ecefea3c23eef2199619
Philip Hutchins <flipture@gmail.com>
2014-04-09 15:36:00 -0400

    adding layer-rails recipes

---
+++ base-opsworks/recipes/rails-app.rb
@@ -0,0 +1,2 @@
+include_recipe 'base-opsworks::monitoring-app'
+include_recipe 'base-opsworks'
--- corndog-chef/recipes/layer-rails-deploy.rb
+++ corndog-chef/recipes/layer-rails-deploy.rb
@@ -1,3 +1,2 @@
 include_recipe "base-opsworks::rails-app"
-include_recipe "unicorn::config-nginx"
 include_recipe "stack_init"
