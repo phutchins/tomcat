c50b7706da1cee5380b4876590f76f12de9b5426
Philip Hutchins <flipture@gmail.com>
2014-04-09 15:59:06 -0400

    cleaning up

--- base-opsworks/recipes/monitoring.rb
+++ base-opsworks/recipes/monitoring.rb
@@ -1,5 +1,3 @@
-#include_recipe 'sar'
-
+include_recipe 'sar'
 include_recipe 'base-opsworks::monitoring-collectd'
 include_recipe 'base-opsworks::monitoring-logstash'
-#include_recipe 'base-opsworks::monitoring-newrelic'
