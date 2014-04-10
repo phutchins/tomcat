6530d2f5c8d09b632b21f15ff6232aee7490e6d4
Jason Gerry <jason.gerry@gmail.com>
2014-04-10 10:09:43 -0400

    change ref method for logstash service

--- config/recipes/logstash-app.rb
+++ config/recipes/logstash-app.rb
@@ -28,7 +28,7 @@ template 'logstash.conf' do
   group node['logstash']['group']
   variables(:stack_name => stack_name, :host_role => host_role)
   mode '0644'
-  notifies :restart, logstash_service_resource
+  notifies :restart, service[logstash_logstash_agent]
 end
 
 
