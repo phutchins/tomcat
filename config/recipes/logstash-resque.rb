cd7f1bcbf8babb710bae07f3e243a2f85da16c49
Jason Gerry <jason.gerry@gmail.com>
2014-04-10 13:35:14 -0400

    separate logstash app config from resque confg

---
+++ config/recipes/logstash-resque.rb
@@ -0,0 +1,21 @@
+include_recipe 'logstash::agent'
+
+node.default['corndog']['host_role'] = 'resque'
+node.default['corndog']['profile'] = 'production'
+
+stack_name = node['corndog']['stack']
+profile_name = node['corndog']['profile']
+host_role = node['corndog']['host_role']
+
+# let's include a normal, sane, erb template for config
+template 'logstash.conf' do
+  source 'logstash-resque.conf.erb'
+  path '/opt/logstash/agent/etc/conf.d/logstash.conf'
+  owner node['logstash']['user']
+  group node['logstash']['group']
+  variables(:stack_name => stack_name, :host_role => host_role)
+  mode '0644'
+  notifies :restart, "service[logstash_agent]"
+end
+
+
