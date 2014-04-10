75e67a688a7ed2a531b958af90512c5bd94e5a36
Jason Gerry <jason.gerry@gmail.com>
2014-04-10 09:48:15 -0400

    fix reference to servicie resource in logstash cookbook

--- config/recipes/logstash-app.rb
+++ config/recipes/logstash-app.rb
@@ -28,7 +28,7 @@ template 'logstash.conf' do
   group node['logstash']['group']
   variables(:stack_name => stack_name, :host_role => host_role)
   mode '0644'
-  notifies :restart, service_resource
+  notifies :restart, logstash_service_resource
 end
 
 
