00b2ed25724dc56d36aa6542568607a3ea706184
Jason Gerry <jason.gerry@gmail.com>
2014-04-11 15:12:50 -0400

    Revert "change logstash setup user"
    This reverts commit fdc91dd687eef7f72d55b71d1a99b900aedac930.

--- setup/recipes/logstash.rb
+++ setup/recipes/logstash.rb
@@ -2,8 +2,6 @@
 
 node.default['logstash']['agent']['version'] = '1.3.3'
 #node.default['logstash']['agent']['server_ipaddress'] = "logs.dealermatch.biz"
-node.override['logstash']['user'] = 'deploy'
-node.override['logstash']['group'] = 'www-data'
 
 include_recipe "logstash::agent"
 
