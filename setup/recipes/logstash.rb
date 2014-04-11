fdc91dd687eef7f72d55b71d1a99b900aedac930
Jason Gerry <jason.gerry@gmail.com>
2014-04-11 15:04:36 -0400

    change logstash setup user

--- setup/recipes/logstash.rb
+++ setup/recipes/logstash.rb
@@ -2,6 +2,8 @@
 
 node.default['logstash']['agent']['version'] = '1.3.3'
 #node.default['logstash']['agent']['server_ipaddress'] = "logs.dealermatch.biz"
+node.override['logstash']['user'] = 'deploy'
+node.override['logstash']['group'] = 'www-data'
 
 include_recipe "logstash::agent"
 
