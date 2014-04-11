e47c7805cf77648ff27bef09aec7934fe9512d55
Philip Hutchins <flipture@gmail.com>
2014-04-11 15:09:36 -0400

    adding changes for logstash user

--- base-opsworks/recipes/monitoring-logstash-app.rb
+++ base-opsworks/recipes/monitoring-logstash-app.rb
@@ -2,15 +2,24 @@
 node.default['corndog']['host_role'] = 'rails_app'
 node.default['corndog']['profile'] = 'production'
 
+node.override['logstash']['user'] = 'deploy'
+node.override['logstash']['supervisor_gid'] = 'www-data'
+
 stack_name = node['corndog']['stack']
 profile_name = node['corndog']['profile']
 host_role = node['corndog']['host_role']
+
+directory "/var/log/nginx" do
+  owner 'www-data'
+  group 'root'
+  mode 0755
+end
 
 script "make logs readable" do
   interpreter "bash"
   user "root"
   cwd "/var/log"
-  code "chmod -R 664 /var/log/nginx"
+  code "chmod 664 /var/log/nginx/*"
 end
 
 node.override[:logstash] = {
@@ -22,34 +31,57 @@ node.override[:logstash] = {
   :agent => {
     :inputs => [
       { :file => {
+          :type => "catchall",
+          :path => [ '/srv/www/corndog/shared/log/*.log' ],
+          :exclude => [ "#{profile_name}.log", "unicorn*log", "salesforce*",
+                        "resque*.log", "mail_delivery.log", "newrelic*",
+                        "cron*", "bluepill*" ],
+          :tags => [ 'catchall' ]
+      } },
+      { :file => {
           :type => "rails",
           :path => [ "/srv/www/corndog/shared/log/#{profile_name}.log" ],
-          :tags => [ 'rails','opsworks' ]
+          :tags => [ 'rails' ]
       } },
       { :file => {
           :type => "unicorn",
           :path => [ '/srv/www/corndog/shared/log/unicorn.stderr.log' ],
-          :tags => [ 'unicorn', 'error','opsworks' ]
+          :tags => [ 'unicorn', 'error' ]
       } },
       { :file => {
           :type => "unicorn",
           :path => [ '/srv/www/corndog/shared/log/unicorn.stdout.log' ],
-          :tags => [ 'unicorn', 'access','opsworks' ]
-      } },
-      { :file =>{
-          :type => "nginx-error",
-          :path => [ '/var/log/nginx/corndog.error.log' ],
-          :tags => [ 'nginx','error','opsworks' ]
+          :tags => [ 'unicorn', 'access' ]
       } },
       { :file => {
           :type => "salesforce_offer_thread",
           :path => [ '/srv/www/corndog/shared/log/salesforce_offer_thread_monitor.log' ],
-          :tags => [ 'salesforce','offer_thread','opsworks' ]
+          :tags => [ 'salesforce','offer_thread' ]
+      } },
+      { :file => {
+          :type => "resque",
+          :path => [ '/srv/www/corndog/shared/log/resque*.log' ],
+          :tags => [ 'resque' ]
+      } },
+      { :file => {
+          :type => "mail_delivery",
+          :path => [ '/srv/www/corndog/shared/log/mail_delivery.log' ],
+          :tags => [ 'mail_delivery' ]
+      } },
+      { :file =>{
+          :type => "nginx-error",
+          :path => [ '/var/log/nginx/*error*.log' ],
+          :tags => [ 'nginx','error' ]
       } },
       { :file => {
           :type => "nginx-access",
-          :path => [ '/var/log/nginx/corndog.access.log' ],
-          :tags => [ 'nginx','access','opsworks' ]
+          :path => [ '/var/log/nginx/*access*.log' ],
+          :tags => [ 'nginx','access' ]
+      } },
+      { :file => {
+          :type => "deploy",
+          :path => [ '/var/lib/aws/opsworks/chef/*.log' ],
+          :tags => [ 'deploy','chef' ]
       } }
     ],
     :filters => [
@@ -79,14 +111,35 @@ node.override[:logstash] = {
       { :condition => 'if "nginx" in [tags] and "access" in [tags]',
         :block => {
           :grok => {
-            :match => [ 'message', '%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \\[%{HTTPDATE:timestamp}\\] "(?:%{WORD:verb} %{NOTSPACE:request}(?: HTTP/%{NUMBER:httpversion})?|%{DATA:rawrequest})" %{NUMBER:response} (?:%{NUMBER:bytes}|-) %{QS:referrer} %{QS:agent} %{QS:forwardedfor} %{NUMBER:timing}' ]
+            :match => [ 'message', '%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \\[%{HTTPDATE:timestamp}\\] \"(?:%{WORD:verb} %{NOTSPACE:request}(?: HTTP/%{NUMBER:httpversion})?|%{DATA:rawrequest})\" %{NUMBER:response} (?:%{NUMBER:bytes}|-) %{QS:referrer} %{QS:agent} %{QS:forwardedfor} %{NUMBER:timing}' ]
+          },
+          :date => {
+            :match => [ "timestamp", "dd/MMM/YYYY:HH:mm:ss Z" ],
+            :add_tag => "ts"
           }
         } },
 
-      { :date => {
-          :match => [ "timestamp", "dd/MMM/YYYY:HH:mm:ss Z" ]
-        }
-      },
+      { :condition => 'if "nginx" in [tags] and "error" in [tags]',
+        :block => {
+          :grok => {
+            :match => [ 'message', '%{GREEDYDATA:timestamp} \\[%{DATA:loglevel}\\] %{GREEDYDATA:errmsg}' ]
+          },
+          :date => {
+            :match => [ "timestamp", "YYYY/MM/dd HH:mm:ss" ],
+            :add_tag => "ts"
+          }
+        } },
+
+      { :condition => 'if [type] == "deploy"',
+        :block => {
+          :grok => {
+            :match => [ 'message', '\\[%{TIMESTAMP_ISO8601:timestamp}\\] %{DATA:level}: %{GREEDYDATA:msg}' ]
+          },
+          :date => {
+            :match => [ "timestamp", "ISO8601" ],
+            :add_tag => "ts"
+          }
+        } },
 
       { :mutate => {
           :add_tag => [ "#{stack_name}", "#{host_role}" ]
@@ -94,7 +147,7 @@ node.override[:logstash] = {
     ],
     :outputs => [
       :redis => {
-        :host => 'logs.dealermatch.biz',
+        :host => 'logstash-redis.dealermatch.biz',
         :port => 16379,
         :data_type => "list",
         :key => "logstash"
