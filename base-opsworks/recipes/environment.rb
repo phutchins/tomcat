0d84367a7a98525c29977f6722a74f1e29582ef7
Philip Hutchins <flipture@gmail.com>
2014-04-14 14:32:09 -0400

    adding bashrc file

--- base-opsworks/recipes/environment.rb
+++ base-opsworks/recipes/environment.rb
@@ -1,3 +1,10 @@
 ## env stuff goes here
 ## bashrc, htoprc, gemrc, etc
 
+template "/home/deploy/.bashrc" do
+  source "bashrc.erb"
+  owner "deploy"
+  group "www-data"
+  mode 0644
+  action :create
+end
