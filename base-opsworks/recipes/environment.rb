18b1500a5714062b32a8d2f0f7669c0bf47d1bb3
Philip Hutchins <flipture@gmail.com>
2014-04-14 14:35:07 -0400

    removing bashrc changes from this repo

--- base-opsworks/recipes/environment.rb
+++ base-opsworks/recipes/environment.rb
@@ -1,10 +1,3 @@
 ## env stuff goes here
 ## bashrc, htoprc, gemrc, etc
 
-template "/home/deploy/.bashrc" do
-  source "bashrc.erb"
-  owner "deploy"
-  group "www-data"
-  mode 0644
-  action :create
-end
