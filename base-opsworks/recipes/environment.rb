807522aa4d0a404da480c21961eb7ab4d2058586
Philip Hutchins <flipture@gmail.com>
2014-04-16 11:44:46 -0400

    adding conditional to creation of bashrc for deploy user

--- base-opsworks/recipes/environment.rb
+++ base-opsworks/recipes/environment.rb
@@ -1,3 +1,11 @@
 ## env stuff goes here
 ## bashrc, htoprc, gemrc, etc
 
+cookbook_file "/home/deploy/.bashrc" do
+  source "bashrc.deploy"
+  mode "0644"
+  owner "deploy"
+  group "www-data"
+  action :create
+  only_if { File.exist?("/home/deploy") }
+end
