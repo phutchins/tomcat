5ee519ebc33d4257d9b019b690a2315795d7020f
Philip Hutchins <flipture@gmail.com>
2014-04-21 10:50:07 -0400

    adding our own repository

--- corndog-chef/recipes/nginx-setup.rb
+++ corndog-chef/recipes/nginx-setup.rb
@@ -1,36 +1,40 @@
 include_recipe 's3_file'
 
 nginx_packages = {
-  "nginx-full" => "nginx-full_1.4.6-precise_amd64.deb",
-  "nginx-common" => "nginx-common_1.4.6-precise_all.deb",
-  "nginx" => "nginx_1.4.6-precise_all.deb"
+  #"nginx-full" => "nginx-full_1.4.6-precise_amd64.deb",
+  #"nginx-common" => "nginx-common_1.4.6-precise_all.deb",
+  #"nginx" => "nginx_1.4.6-precise_all.deb"
+  "init-system-helpsers" => "1.7",
+  "nginx-full" => "1.4.6-1+precise0",
+  "nginx-common" => "1.4.6-1+precise0",
+  "nginx" => "1.4.6-1+precise0"
 }
 
-nginx_packages.each do |pkg_name, file_name|
-  s3_file "/var/cache/apt/archives/#{file_name}" do
-    remote_path "/#{file_name}"
-    bucket "cdx-apt-repo"
-    aws_access_key_id "AKIAJ4TP2AWG2GYM6V6Q"
-    aws_secret_access_key "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"
-    not_if { File.exists?("/var/cache/apt/archives/#{file_name}") }
-  end
-end
-
-# Disabling this as the repo does not provide the version we use any more
-#apt_repository "nginx-stable-precise" do
-#  uri "http://ppa.launchpad.net/nginx/stable/ubuntu"
-#  distribution "precise"
-#  components ["main"]
-#  keyserver "keyserver.ubuntu.com"
-#  key "C300EE8C"
-#  action :add
-#  notifies :run, "execute[apt-get update]", :immediately
+#nginx_packages.each do |pkg_name, file_name|
+#  s3_file "/var/cache/apt/archives/#{file_name}" do
+#    remote_path "/#{file_name}"
+#    bucket "cdx-apt-repo"
+#    aws_access_key_id "AKIAJ4TP2AWG2GYM6V6Q"
+#    aws_secret_access_key "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"
+#    not_if { File.exists?("/var/cache/apt/archives/#{file_name}") }
+#  end
 #end
 
-nginx_packages.each do |pkg_name, file_name|
+apt_repository "DM_Apt_Repo" do
+  uri "http://apt-repo.dealermatch.biz/repo"
+  distribution "precise"
+  components ["main"]
+#  keyserver "keyserver.ubuntu.com"
+#  key "C300EE8C"
+  action :add
+  notifies :run, "execute[apt-get update]", :immediately
+end
+
+nginx_packages.each do |pkg_name, pkg_version|
   package pkg_name do
     action :install
-    source "/var/cache/apt/archives/#{file_name}"
+    version pkg_version
+#    source "/var/cache/apt/archives/#{file_name}"
     options "-o Dpkg::Options::='--force-depends','--force-confold'"
   end
 end
@@ -40,9 +44,3 @@ end
 #  source "/var/cache/apt/archives/init-system-helpers_1.7~precise1~ppa1_all.deb"
 #  version "1.7~precise1~ppa1"
 #end
-
-execute 'install_init_system_helpers' do
-  command 'dpkg -i /var/cache/apt/archives/init-system-helpers_1.7~precise1~ppa1_all.deb'
-  action :run
-  only_if { File.exists?("/var/cache/apt/archives/init-system-helpers_1.7~precise1~ppa1_all.deb") }
-end
