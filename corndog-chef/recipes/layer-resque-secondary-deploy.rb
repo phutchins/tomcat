1583e30ab019b0ab9c6ca100db8f73cd1f4f5746
Philip Hutchins <flipture@gmail.com>
2014-04-25 15:47:44 -0400

    fixing resque

--- corndog-chef/recipes/layer-resque-secondary-deploy.rb
+++ corndog-chef/recipes/layer-resque-secondary-deploy.rb
@@ -1,5 +1,3 @@
 include_recipe 'base-opsworks'
 include_recipe 'base-opsworks::resque'
 include_recipe 'corndog-chef::resque-deploy'
-# Commenting out until we're done testing
-#include_recipe 'corndog-chef::resque-cron'
