f282bc0d065bcd079efd1deb53ad05bc0852b207
Philip Hutchins <flipture@gmail.com>
2014-04-26 09:12:03 -0400

    adding stack init to setup to fix precompile issue on new hosts

--- corndog-chef/recipes/layer-rails-configure.rb
+++ corndog-chef/recipes/layer-rails-configure.rb
@@ -1,3 +1,3 @@
-include_recipe "corndog-chef::nginx-setup"
-include_recipe "base-opsworks::rails-app"
-include_recipe "corndog-chef::stack-init"
+include_recipe 'corndog-chef::nginx-setup'
+include_recipe 'base-opsworks::rails-app'
+include_recipe 'corndog-chef::stack-init'
--- corndog-chef/recipes/layer-rails-setup.rb
+++ corndog-chef/recipes/layer-rails-setup.rb
@@ -1,2 +1,3 @@
 include_recipe 'corndog-chef::app-packages'
 include_recipe 'corndog-chef::nginx-setup'
+include_recipe 'corndog-chef::stack-init'
