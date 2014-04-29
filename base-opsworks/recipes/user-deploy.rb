d654191de7d87e056d3cad0efff54525e73bc45f
Philip Hutchins <flipture@gmail.com>
2014-04-29 08:31:50 -0400

    adding .ssh directory creation

--- base-opsworks/recipes/user-deploy.rb
+++ base-opsworks/recipes/user-deploy.rb
@@ -6,6 +6,12 @@ cookbook_file "/etc/sudoers.d/deploy" do
   action :create
 end
 
+directory "/home/deploy/.ssh" do
+  mode 0700
+  owner "deploy"
+  action :create
+end
+
 cookbook_file "/home/deploy/.ssh/authorized_keys" do
   source "authorized_keys.deploy"
   mode 0400
