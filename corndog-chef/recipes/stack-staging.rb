4179d26f2e716d92d7c57bf50b507f0997e04d6a
Jason Gerry <jason.gerry@gmail.com>
2014-05-14 14:43:28 -0400

    updated staging with new CF distro

--- corndog-chef/recipes/stack-staging.rb
+++ corndog-chef/recipes/stack-staging.rb
@@ -5,7 +5,7 @@ run_context.include_recipe 'corndog-chef::stack-defaults'
 # This file includes all attributes that apply only to the devopstest stack
 
 # Assign environmentals to node
-node.normal['corndog']['cloudfront']['asset_hosts'] = 'https://d37di6rf5nv9f5.cloudfront.net'
+node.normal['corndog']['cloudfront']['asset_hosts'] = 'https://d2tyyywupxeit9.cloudfront.net'
 
 node.normal['corndog']['mongodb']['host_port_1'] = 'cde-staging.m0.mongolayer.com:27017'
 node.normal['corndog']['mongodb']['database'] = 'cde_staging'
