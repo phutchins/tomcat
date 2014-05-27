0fdb5f6caf6c57d5382948d5da23f7ef019d6c77
Philip Hutchins <flipture@gmail.com>
2014-05-27 13:39:49 -0400

    uncommenting cron from cron recipe as we have a nocron and a cron recipe now

--- corndog-chef/recipes/layer-resque-deploy-cron.rb
+++ corndog-chef/recipes/layer-resque-deploy-cron.rb
@@ -1,6 +1,4 @@
-include_recipe 'base-opsworks'
 include_recipe 'base-opsworks::resque'
 include_recipe 'corndog-chef::resque-deploy'
 include_recipe 'corndog-chef::resque-service'
-#commenting out until we are done testing
-#include_recipe 'corndog-chef::resque-cron'
+include_recipe 'corndog-chef::resque-cron'
