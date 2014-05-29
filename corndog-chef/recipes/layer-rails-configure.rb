7114811f3352355d02d68320e1ffba4228cb5245
Philip Hutchins <flipture@gmail.com>
2014-05-29 13:25:37 -0400

    cleaning up unused nginx config file and adding nginx custom logging back

--- corndog-chef/recipes/layer-rails-configure.rb
+++ corndog-chef/recipes/layer-rails-configure.rb
@@ -1,3 +1,4 @@
 include_recipe 'corndog-chef::nginx-setup'
+include_recipe 'corndog-chef::nginx-config'
 include_recipe 'base-opsworks::rails-app'
 include_recipe 'corndog-chef::stack-init'
--- corndog-chef/recipes/nginx-config.rb
+++ corndog-chef/recipes/nginx-config.rb
@@ -1,22 +1,11 @@
 # nginx config
-#cookbook_file "log_format" do
-#  path '/etc/nginx/conf.d/log_format.conf'
-#  owner 'root'
-#  group 'root'
-#  mode 0644
-#  notifies :restart, "service[nginx]"
-#end
-
-#template "/etc/nginx/sites-available/corndog" do
-#  owner 'root'
-#  group 'root'
-#  mode 0644
-#  source 'nginx.conf.erb'
-#  variables({
-#    :hostname => node[:opsworks][:instance][:hostname]
-#  })
-#  notifies :restart, "service[nginx]"
-#end
+cookbook_file "log_format" do
+  path '/etc/nginx/conf.d/log_format.conf'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :restart, "service[nginx]"
+end
 
 #service "nginx" do
 #  action [ :enable, :start ]
