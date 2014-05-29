700de8d61f722f3d6350bce0227d65a9ab747e66
Philip Hutchins <flipture@gmail.com>
2014-05-29 13:41:51 -0400

    trying attribute to add log_format

--- corndog-chef/recipes/nginx-config.rb
+++ corndog-chef/recipes/nginx-config.rb
@@ -1,11 +1,11 @@
 # nginx config
-cookbook_file "log_format" do
-  path '/etc/nginx/conf.d/log_format.conf'
-  owner 'root'
-  group 'root'
-  mode 0644
-  notifies :restart, "service[nginx]"
-end
+#cookbook_file "log_format" do
+#  path '/etc/nginx/conf.d/log_format.conf'
+#  owner 'root'
+#  group 'root'
+#  mode 0644
+#  notifies :restart, "service[nginx]"
+#end
 
 #service "nginx" do
 #  action [ :enable, :start ]
