fb3d6b1691bcd83f967d8bbc70b88fcf7e07c121
Philip Hutchins <flipture@gmail.com>
2014-05-29 14:30:07 -0400

    removing extra semicolon from log_format

--- nginx/attributes/customize.rb
+++ nginx/attributes/customize.rb
@@ -1,1 +1,1 @@
-normal[:nginx][:log_format]['main'] = "'$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" \"$http_x_forwarded_for\" $request_time';"
+normal[:nginx][:log_format]['main'] = "'$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" \"$http_x_forwarded_for\" $request_time'"
