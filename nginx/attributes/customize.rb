dcf5319a735fc32718b35f9f8641fb403a548484
Philip Hutchins <flipture@gmail.com>
2014-05-29 14:06:49 -0400

    fixing log_format

--- nginx/attributes/customize.rb
+++ nginx/attributes/customize.rb
@@ -1,1 +1,1 @@
-node.default[:nginx][:log_format] = "'$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" \"$http_x_forwarded_for\" $request_time';"
+normal[:nginx][:log_format]['main'] = "'$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" \"$http_x_forwarded_for\" $request_time';"
