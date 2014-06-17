35e432018898eede0edcfe1287ed308beb79eeaf
Philip Hutchins <flipture@gmail.com>
2014-06-17 09:47:20 -0400

    fixing comparison

--- corndog-chef/recipes/layer-solr-master-configure.rb
+++ corndog-chef/recipes/layer-solr-master-configure.rb
@@ -1,7 +1,7 @@
 include_recipe 'base-opsworks'
 
 node.default[:solr][:replication][:node_type] = "master"
-(node[:corndog][:stack] = "production") ? domain = "dealermatch.com" : domain = "dealermatch.biz"
+(node[:corndog][:stack] = "production") ? domain == "dealermatch.com" : domain = "dealermatch.biz"
 node.default[:solr][:replication][:master_url] = "solr-#{node[:corndog][:stack]}.#{domain}"
 
 include_recipe 'chef-solr'
