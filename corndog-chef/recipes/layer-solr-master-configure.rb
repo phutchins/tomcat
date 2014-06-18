e24b6ea8950c4ac2cccc33f08397816661add557
Philip Hutchins <flipture@gmail.com>
2014-06-18 13:11:52 -0400

    adding bits for replication settings

--- corndog-chef/recipes/layer-solr-master-configure.rb
+++ corndog-chef/recipes/layer-solr-master-configure.rb
@@ -1,7 +1,5 @@
 include_recipe 'base-opsworks'
 
 node.default[:solr][:replication][:node_type] = "master"
-(node[:corndog][:stack] == "production") ? domain = "dealermatch.com" : domain = "dealermatch.biz"
-node.default[:solr][:replication][:master_url] = "solr-#{node[:corndog][:stack]}.#{domain}"
 
 include_recipe 'corndog-chef::solr-config'
