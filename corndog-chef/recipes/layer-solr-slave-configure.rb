c1e0c2ca73a2914ec609135623715eb0ad5a8e06
Philip Hutchins <flipture@gmail.com>
2014-06-18 11:44:01 -0400

    setting index for solr dns name

--- corndog-chef/recipes/layer-solr-slave-configure.rb
+++ corndog-chef/recipes/layer-solr-slave-configure.rb
@@ -3,7 +3,7 @@ node.default[:solr][:replication][:node_type] = "slave"
 
 if !node['opsworks']['layers']['solr']['instances'].nil?
   solr_opsworks_instance = node['opsworks']['layers']['solr']['instances'].first
-  solr_master_dns = solr_opsworks_instance['public_dns_name']
+  solr_master_dns = solr_opsworks_instance[1]['public_dns_name']
 end
 node.default[:solr][:replication][:master_url] = "http://#{solr_master_dns}:#{node[:solr][:port].to_s}/solr"
 
