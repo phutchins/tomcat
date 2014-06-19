c310f53a837cfc7a95be003a12f6eaca0bf87153
Philip Hutchins <flipture@gmail.com>
2014-06-19 12:14:00 -0400

    adding solr configuration recipes

---
+++ corndog-chef/recipes/solr-production.rb
@@ -0,0 +1,2 @@
+node.normal['solr']['tomcat']['xmx'] = "5120m"
+node.normal['solr']['tomcat']['xms'] = "2048m"
--- corndog-chef/recipes/solr-setup.rb
+++ corndog-chef/recipes/solr-setup.rb
@@ -1,1 +1,2 @@
+include_recipe "corndog-chef::solr-#{node['corndog']['stack']}"
 include_recipe 'chef-solr::install'
