41d25bf1f1072dfe82e18d54bf84c2f57dc92994
Philip Hutchins <flipture@gmail.com>
2014-06-19 12:51:15 -0400

    SOCORE - Commiting changed cookbooks

--- chef-solr/attributes/tomcat.rb
+++ chef-solr/attributes/tomcat.rb
@@ -3,4 +3,4 @@ default['solr']['tomcat']['xmx'] = "512m"
 default['solr']['tomcat']['xms'] = "256m"
 
 # Overrides for tomcat cookbook
-default['tomcat']['base_version'] = "6.0.35-1ubuntu3.4"
+default['tomcat']['base_version'] = "tomcat6"
